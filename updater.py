6742d5488fc817d6b98b536bf558b47dd124b925
bdamja <iceicebabyalter@gmail.com>
2022-07-14 21:17:54 -0600

    wip

---
+++ updater.py
@@ -0,0 +1,50 @@
+import requests
+import os
+from pathlib import Path
+import zipfile
+import shutil
+
+URL = "https://github.com/r0hkx/vine-ssg-practice/releases/download/v1.4.1/Vine.SSG.Practice.v1.4.1-1.16.5.zip"
+CURRENT_PATH = Path(os.path.realpath(__file__))
+WORLD_PATH = CURRENT_PATH.parent.parent.absolute()
+SAVES_PATH = WORLD_PATH.parent.absolute()
+ZIP_PATH = str(SAVES_PATH) + "/VineSSGLatest.zip"
+TMP_PATH = str(SAVES_PATH) + "/tmp/"
+
+def main():
+  download()
+  extract()
+  move_files()
+  
+def download(): 
+   # direct download
+  response = requests.get(URL) # get a response from an http request
+  open(ZIP_PATH, "wb").write(response.content) # write to a new file with the contents of the response (the world zip)
+
+def extract():
+  with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref: # find the world zip
+    zip_ref.extractall(str(TMP_PATH)) # extract the world to a temporary folder
+
+def move_files():
+  new_world_filename = os.listdir(str(TMP_PATH))[0] # get the filename of the only file present in the tmp folder (which should be the world file)
+  new_world_path = Path(TMP_PATH + new_world_filename)
+
+
+
+
+  scoreboard_dat_location = "/data/scoreboard.dat"
+  scoreboard_dat_src = str(WORLD_PATH) + scoreboard_dat_location
+  scoreboard_dat_dst = str(new_world_path) + scoreboard_dat_location
+
+  command_storage_minecraft_dat_location = "/data/command_storage_minecraft.dat"
+  command_storage_minecraft_dat_src = str(WORLD_PATH) + command_storage_minecraft_dat_location
+  command_storage_minecraft_dat_dst = str(new_world_path) + command_storage_minecraft_dat_location
+
+  shutil.copyfile(scoreboard_dat_src, scoreboard_dat_dst)
+  shutil.copyfile(command_storage_minecraft_dat_src, command_storage_minecraft_dat_dst)
+
+  shutil.move(str(WORLD_PATH), str(SAVES_PATH) + "/new")
+
+
+if __name__ == '__main__':
+  main()
