feef9849ac213d7d4cee575cc2e1e62e7cc89b15
bdamja <iceicebabyalter@gmail.com>
2022-07-14 23:33:07 -0600

    resolved issue with scoreboard values being reset

--- updater.py
+++ updater.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import zipfile
 import shutil
 
-URL = "https://github.com/r0hkx/vine-ssg-practice/releases/download/v1.4.1/Vine.SSG.Practice.v1.4.1-1.16.5.zip"
+URL = "https://github.com/r0hkx/vine-ssg-practice/releases/download/current/Vine.SSG.Practice.current-1.16.5.zip"
 CURRENT_PATH = Path(os.path.realpath(__file__))
 WORLD_PATH = CURRENT_PATH.parent.parent.absolute()
 SAVES_PATH = WORLD_PATH.parent.absolute()
@@ -17,15 +17,17 @@ def main():
   move_files()
   
 def download(): 
-   # direct download
+  print("Downloading the latest Vine SSG Practice Map release from https://github.com/r0hkx/vine-ssg-practice/releases...")
   response = requests.get(URL) # get a response from an http request
   open(ZIP_PATH, "wb").write(response.content) # write to a new file with the contents of the response (the world zip)
 
 def extract():
+  print("Extracting the world file to " + str(SAVES_PATH) + "/...")
   with zipfile.ZipFile(ZIP_PATH, 'r') as zip_ref: # find the world zip
     zip_ref.extractall(str(TMP_PATH)) # extract the world to a temporary folder
 
 def move_files():
+  print("Transferring personal best splits and inventory setup to the new map...")
   new_world_filename = os.listdir(str(TMP_PATH))[0] # get the filename of the only file present in the tmp folder (which should be the world file)
   new_world_path = Path(TMP_PATH + new_world_filename)
 
@@ -41,6 +43,7 @@ def move_files():
   shutil.copyfile(command_storage_minecraft_dat_src, command_storage_minecraft_dat_dst) # move the inventory setups over
   shutil.move(str(new_world_path), str(SAVES_PATH) + "/" + str(new_world_filename)) # move the world out to saves
   os.rmdir(TMP_PATH) # delete the tmp folder
+  print("Success!")
 
 if __name__ == '__main__':
   main()
